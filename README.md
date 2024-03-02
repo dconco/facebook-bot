126e7fff70584f0836ca9280ef3b6da199c92083
dconco <onipededavid208@gmail.com>
2024-03-02 16:38:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# facebook-bot
+Dconco AI chatbot on Facebook 
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+// facebook
+$access_token = 'EAAOOom4rGZAQBO3mvNrzcIAR6qeSSk0xBpqV6GxrZCdlwC1ZBQYBRcouF33YasGd3W6OEr7ToVHHLHl2erqDZBZA7ZAOQvJxdNZC68pM3qOz4JLNbFtt47SrLC1lK14QZCxeMboYSUapLjkNILO0wjXAlvGxirflWlQHYRuEfGYNEQzF7uXiGFeIUACsJJ13hZCUz2nHQvuut09aor1aI';
+$my_verify_token = 'davebot123';
+
+// open ai
+$open_ai_secret = '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';
